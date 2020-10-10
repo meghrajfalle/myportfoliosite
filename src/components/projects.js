234fd6b2756e6e947b18bdf5f0a1e497672385d4
meghrajfalle <meghraj.falle@appdirect.com>
2020-10-10 20:58:39 +0530

    Branch3: Adding the code to display the tabs and grid view for each of the projects in the Project Tab. Used Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton from React MDL

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,172 @@
 import React, { Component } from 'react';
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from 'react-mdl';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className='projects-grid'>
+          {/** Project 1 */}
+          <Card shadow={5} style={{ minWidth: '450px', margin: 'auto' }}>
+            <CardTitle
+              style={{
+                color: '#fff',
+                height: '176px',
+                background:
+                  'url(https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe) center / cover'
+              }}
+            >
+              React Project #1 {''}
+            </CardTitle>
+            <CardText>
+              {' '}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name='share' />
+            </CardMenu>
+          </Card>
+          {/** Project 2 */}
+          <Card shadow={5} style={{ minWidth: '450px', margin: 'auto' }}>
+            <CardTitle
+              style={{
+                color: '#fff',
+                height: '176px',
+                background:
+                  'url(https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe) center / cover'
+              }}
+            >
+              React Project #2 {''}
+            </CardTitle>
+            <CardText>
+              {' '}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name='share' />
+            </CardMenu>
+          </Card>
+          {/** Project 3 */}
+          <Card shadow={5} style={{ minWidth: '450px', margin: 'auto' }}>
+            <CardTitle
+              style={{
+                color: '#fff',
+                height: '176px',
+                background:
+                  'url(https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe) center / cover'
+              }}
+            >
+              React Project #3 {''}
+            </CardTitle>
+            <CardText>
+              {' '}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name='share' />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          {/** Project 1 */}
+          <Card shadow={5} style={{ minWidth: '450px', margin: 'auto' }}>
+            <CardTitle
+              style={{
+                color: '#fff',
+                height: '176px',
+                background:
+                  'url(https://www.filepicker.io/api/file/SVFQZQAyRpqJ31f6LNGe) center / cover'
+              }}
+            >
+              Angular Project #1 {''}
+            </CardTitle>
+            <CardText>
+              {' '}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name='share' />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is VueJS</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className='category-tab'>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJS</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className='content'> {this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
